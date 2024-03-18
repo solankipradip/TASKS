4079dcbdf4088a282ecfb58b78dd1e39d00d3db7
solankipradip <solankipradip096@gmail.com>
2024-03-18 07:37:10 +0530

    a

---
+++ Slider 3/js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    $('.owl-carousel').owlCarousel({
+        loop: false,
+        margin: 10,
+        nav: false,
+        dots: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+
+    })
+    
+    
+    
+});
